e7b7d5f98392458974ef70613d6aa9f1a7770dbd
Anil Anar <anilanar@hotmail.com>
2021-10-06 09:58:29 +0200

    fix babel preset env config

--- packages/chakra-ui/babel.config.js
+++ packages/chakra-ui/babel.config.js
@@ -2,6 +2,13 @@ const TARGET_ENV = process.env.TARGET_ENV;
 const isCommonJS = TARGET_ENV === 'cjs';
 const isESM = TARGET_ENV === 'esm';
 
+const targets = {
+  esm: { esmodules: true },
+  test: {
+    node: 'current',
+  },
+};
+
 module.exports = (api) => {
   api.cache(() => `${process.env.NODE_ENV}-${TARGET_ENV}`);
 
@@ -14,8 +21,8 @@ module.exports = (api) => {
           loose: true,
           modules: isCommonJS || api.env('test') ? 'commonjs' : false,
           targets: {
-            esmodules: isESM ? true : undefined,
-            node: api.env('test') ? 'current' : undefined,
+            ...(isESM && targets.esm),
+            ...(api.env('test') && targets.test),
           },
         },
       ],
