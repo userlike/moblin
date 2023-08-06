3f011ddc3153a822a844148b87970dda7888ab83
Anil Anar <anilanar@hotmail.com>
2023-08-06 22:27:37 +0200

    add overflow stories

---
+++ .changeset/loud-chairs-talk.md
@@ -0,0 +1,5 @@
+---
+"@moblin/storybook-chakra-ui": patch
+---
+
+add stories for when flex items won't fit into their container
--- packages/storybook-chakra-ui/src/Flex.stories.tsx
+++ packages/storybook-chakra-ui/src/Flex.stories.tsx
@@ -1,13 +1,11 @@
 import { contentDistributions, contentPositions } from "@moblin/core";
 import { storiesOf } from "@storybook/react";
 
-import { Box, Flex, FlexItem, WithChildren } from "@moblin/chakra-ui";
+import { Box, BoxProps, Flex, FlexItem } from "@moblin/chakra-ui";
 import { demo } from "./animatable-demo";
 
-const Item = ({ children }: WithChildren) => (
-  <Box bg="red.700" color="gray.200">
-    {children}
-  </Box>
+const Item = (props: BoxProps) => (
+  <Box {...props} bg="red.700" color="gray.200" />
 );
 
 const flexStories = (["column", "row"] as const).flatMap((direction) =>
@@ -42,6 +40,58 @@ flexStories.reduce(
     return acc;
   },
   storiesOf("Flex", module)
+    .addDecorator(demo())
+    .addParameters({
+      layout: "centered",
+      docs: {
+        inlineStories: false,
+        iframeHeight: 500,
+      },
+    })
+);
+
+const overflowStories = (["column", "row"] as const).flatMap((direction) =>
+  contentPositions.flatMap((alignItems) =>
+    contentDistributions.map(
+      (justifyContent) =>
+        [
+          `d=${direction[0]} a=${alignItems} j=${justifyContent}`,
+          () => (
+            <Flex
+              direction={direction}
+              alignItems={alignItems}
+              justifyContent={justifyContent}
+              gap={2}
+            >
+              <FlexItem>
+                <Item
+                  w={direction === "row" ? "10rem" : undefined}
+                  h={direction === "column" ? "10rem" : undefined}
+                >
+                  Foo
+                </Item>
+              </FlexItem>
+              <FlexItem>
+                <Item
+                  w={direction === "row" ? "10rem" : undefined}
+                  h={direction === "column" ? "10rem" : undefined}
+                >
+                  Bar
+                </Item>
+              </FlexItem>
+            </Flex>
+          ),
+        ] as [string, () => JSX.Element]
+    )
+  )
+);
+
+overflowStories.reduce(
+  (acc, [name, story]) => {
+    acc.add(name, story);
+    return acc;
+  },
+  storiesOf("Flex (overflow)", module)
     .addDecorator(demo())
     .addParameters({
       layout: "centered",
